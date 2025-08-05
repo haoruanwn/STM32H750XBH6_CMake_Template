f32f75d4e7a002e185c6e3965c0d65ca1b9ecd65
好软，好温暖 <128681465+haoruanwn@users.noreply.github.com>
2025-08-05 23:28:57 +0800

    完善readme 2025.8.5

---
+++ Drivers/User/Inc/test.h
@@ -0,0 +1,12 @@
+//
+// Created by hao on 25-8-5.
+//
+
+#ifndef TEST_H
+#define TEST_H
+
+#include <stdio.h>
+
+void test();
+
+#endif //TEST_H
---
+++ Drivers/User/Src/test.c
@@ -0,0 +1,9 @@
+//
+// Created by hao on 25-8-5.
+//
+#include "test.h"
+
+void test()
+{
+    printf("Hello World!\n");
+}
